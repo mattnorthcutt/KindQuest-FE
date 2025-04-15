ea82bb9d0d6ae870c3a18174d433eaa2fb006f01
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-04-14 21:54:41 -0400

    volunteers page

---
+++ src/app/volunteers/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+function VolunteersPage() {
+  return (
+    <div className="text-center my-4">
+      <div className="d-flex flex-wrap">
+        <h1>Lorem Ipsum</h1>
+      </div>
+    </div>
+  );
+}
+
+export default VolunteersPage;
