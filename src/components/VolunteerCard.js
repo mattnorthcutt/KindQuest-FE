1401abfe63418e9f3fd2d01ca65533f2d3338b57
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-04-19 11:25:16 -0400

    created a volunteer card, updated volunteers page with sample data

---
+++ src/components/VolunteerCard.js
@@ -0,0 +1,39 @@
+'use client';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+
+function VolunteerCard({ volunteerObj }) {
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={volunteerObj.image || '/images/default-avatar.png'} alt={`${volunteerObj.firstName} ${volunteerObj.lastName}`} />
+      <Card.Body>
+        <Card.Title>{`${volunteerObj.firstName} ${volunteerObj.lastName}`}</Card.Title>
+        <Card.Text>
+          Email:
+          <a href={`mailto:${volunteerObj.email}`}>{volunteerObj.email}</a>
+        </Card.Text>
+        <h5>Projects:</h5>
+        <ul>
+          {volunteerObj.projects.map((project) => (
+            <li key={project.firebaseKey}>{project.name}</li>
+          ))}
+        </ul>
+      </Card.Body>
+    </Card>
+  );
+}
+
+VolunteerCard.propTypes = {
+  volunteerObj: PropTypes.shape({
+    image: PropTypes.string,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    projects: PropTypes.arrayOf(PropTypes.string).isRequired,
+    firebaseKey: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default VolunteerCard;
